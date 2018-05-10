60ddab80945da48ae95a2e50259773635155089f
Evilbas <dimonloboto@gmail.com>
2018-05-10 08:27:28 -0400

    Init

---
+++ src/main/java/com/evilbas/dyndbcache/App.java
@@ -0,0 +1,13 @@
+package com.evilbas.dyndbcache;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
